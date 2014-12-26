c49d53faa12854f011a2a0e5bc3c3efbfd61b0e5
KonstGit <kostyan.vk@gmail.com>
2014-12-26 23:47:16 +0200

    + reflect

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -4,27 +4,37 @@
  * and open the template in the editor.
  */
 
+import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  *
  * @author Konst
  */
 public class First {
-    
+
     public First() {
     }
-    
-    
+
     @Test
-    public void test1() {
-        System.out.println("Tests are fucking good!!!");
+    public void test1() throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.get("http://www.ranorex.com/web-testing-examples/vip");
+        Thread.sleep(5000);
+        System.out.println("good");
+        driver.quit();
     }
-    
+
     @Test
     public void test2() {
         System.out.println("ooouueee theq are really good!!!");
     }
-    
+
 }
