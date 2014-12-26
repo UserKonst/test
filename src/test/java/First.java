5c37c1c687811e041ab5a1eee489936fe19156a5
KonstGit <kostyan.vk@gmail.com>
2014-12-26 23:29:11 +0200

    + reflect

---
+++ src/test/java/First.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Konst
+ */
+public class First {
+    
+    public First() {
+    }
+    
+    
+    @Test
+    public void test1() {
+        System.out.println("Tests are fucking good!!!");
+    }
+    
+    @Test
+    public void test2() {
+        System.out.println("ooouueee theq are really good!!!");
+    }
+    
+}
